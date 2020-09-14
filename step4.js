fe998be5e957c97ffe1ff6535f65f31925c4816d
Toby-Yuan <68837496+Toby-Yuan@users.noreply.github.com>
2020-09-14 12:30:21 +0800

    vue

--- step4.js
+++ step4.js
@@ -7,3 +7,29 @@ var app = new Vue({
         console.log('a is:' + this.a)
     }
 })
+
+app.a = 'change';
+
+var app1 = new Vue({
+    el: '#app1',
+    data: {
+        rawHtml: '<span style="color: red">This should be red</span>'
+    }
+})
+
+var app2 = new Vue({
+    el: '#app2',
+    data: {
+        a: 3,
+        ok: true,
+        message: 'It will be reverse',
+        id: 1
+    }
+})
+
+var app3 = new Vue({
+    el: '#app3',
+    data: {
+        seen: true
+    }
+})
