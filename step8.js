1cbdf3759cd2551c468f948b492b624699649fe7
toby <toby@mail>
2020-09-15 10:56:35 +0800

    vue

--- step8.js
+++ step8.js
@@ -7,3 +7,25 @@ var app = new Vue({
         ]
     }
 })
+
+var app1 = new Vue({
+    el: '#app1',
+    data: {
+        parentMessage: 'parent',
+        items: [
+            {message: 'test1'},
+            {message: 'test2'}
+        ]
+    }
+})
+
+var app2 = new Vue({
+    el: '#app2',
+    data: {
+        object: {
+            title: 'How to do lists in Vue',
+            author: 'Jane Done',
+            public: '2020-08-10'
+        }
+    }
+})
