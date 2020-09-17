4c2bfed05f15fe347d6b16a0b616779ba1d67e69
toby <toby@mail>
2020-09-17 15:21:55 +0800

    Vue

---
+++ step10.js
@@ -0,0 +1,27 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        msg: 'Hello'
+    }
+})
+
+var app1 = new Vue({
+    el: '#app1',
+    data: {
+        checked: false
+    }
+})
+
+var app2 = new Vue({
+    el: '#app2',
+    data: {
+        buyList: []
+    }
+})
+
+var app3 = new Vue({
+    el: '#app3',
+    data: {
+        checkOne: ''
+    }
+})
